8f502756d58156649034678dbb5acc2012ebdfc0
Alon G <agoykman@gmail.com>
2021-11-30 03:19:58 +0200

    fix: fsPromises

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,23 @@
 const router = require('express').Router();
-const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 
 const usersPath = path.join('data', 'users.json');
 
-router.get('/users', (req, res) => {
-  fs.readFile(usersPath, (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    res.status(200).send(data);
-  });
-});
+fsPromises.readFile(usersPath)
+  .then((data) => {
+    router.get('/users', (req, res) => {
+      res.status(200).send(data);
+    });
 
-router.get('/users/:id', (req, res) => {
-  fsPromises.readFile(usersPath)
-    .then((data) => {
+    router.get('/users/:id', (req, res) => {
       const searchUser = JSON.parse(data).filter((user) => user._id === req.params.id);
       if (!searchUser.length) {
         res.status(404).send({ message: 'User ID not found' });
         return;
       }
       res.send(searchUser);
-    }).catch((err) => {
-      console.log(err);
     });
-});
+  }).catch((err) => { console.log(err); });
 
 module.exports = router;
